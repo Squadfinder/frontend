81c683440a8a95416f53bdd3a494247691fd48c1
Thomas Fettig <thomasmfettig@gmail.com>
2022-10-16 15:26:51 -0600

    Update styling to main page, create mock data file

---
+++ mock-data/mock-game-data.js
@@ -0,0 +1,16 @@
+let games = [
+  {
+    title: 'Halo 2',
+    image: 'https://upload.wikimedia.org/wikipedia/en/9/92/Halo2-cover.png',
+    genre: ['Shooter', 'Adventure'],
+    consoles: ['Xbox', 'PS5', 'PC']
+  },
+  {
+    title: 'Fall Guys',
+    image: 'https://d1x3t653etfkkb.cloudfront.net/games/featured-game-blocks/_size640x560/FB_GM.75_0620_L.png',
+    genre: ['Battle Royale', 'Obstacle Course'],
+    consoles: ['Xbox', 'PS5', 'PC', 'Switch']
+  }
+]
+
+export { games }
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,8 +2,23 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, Pressable, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import Swiper from 'react-native-swiper';
+import { games } from '../../mock-data/mock-game-data';
 
 import NavMenu from "./NavMenu";
+
+let userGames = games.map(game => {
+  return (
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <Image
+        source={{uri: game.image}}
+        style={{height: 325, width: 250, borderWidth: 5, borderColor: 'black', borderRadius: 20}}>
+      </Image>
+      <Button
+        title='Form a Squad'
+      />
+    </View>
+  )
+})
 
 const HomeScreen = ({ navigation }) => {
   return (
@@ -15,24 +30,14 @@ const HomeScreen = ({ navigation }) => {
         <Text>My Games:</Text>
       </View>
       <Swiper style={styles.swiper} showsButtons={true}>
-        <View>
-          <Text style={styles.slide1}>Slide1</Text>
-          <Button
-            title="Form a Squad"
-          />
-        </View>
-        <View>
-          <Text style={styles.slide2}>Slide2</Text>
-          <Button
-            title="Form a Squad"
-          />
-        </View>
+        {userGames}
       </Swiper>
-      <StatusBar style="auto" />
       <Button
+        style={styles.editButton}
         title="Edit My Games"
         onPress={() => navigation.navigate("Profile")}
       />
+      <Text style={styles.rawg}>Powered by RAWG</Text>
     </View>
   );
 };
@@ -40,37 +45,34 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#393051",
     alignItems: "center",
   },
   header: {
     height: 55,
     width: '100%',
     fontSize: 35,
-    backgroundColor: '#000',
-    color: '#fff',
+    backgroundColor: '#483F6D',
+    color: '#3AE456',
     textAlign: 'center',
+    marginBottom: 10,
   },
   info: {
     height: 150,
-    backgroundColor: '#3AE456',
+    backgroundColor: '#483F6D',
     width: 250,
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: '#3AE456',
+    borderWidth: 5,
     borderRadius: 50,
+    marginBottom: 10,
   },
   swiper: {},
-  slide1: {
-    width: 150,
-    height: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red'
-  },
-  slide2: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue',
+  editButton: {},
+  rawg: {
+    marginTop: 10,
+    marginBottom: 10,
   }
 });
 
