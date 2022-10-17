52f0fcfac226393718d79a07555c398d573338f3
Nick Liggett <nick.liggett@gmail.com>
2022-10-17 11:38:07 -0600

    Started the MyGames and SearchGames Views

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import HomeScreen from "./src/components/HomeScreen";
 import ProfileScreen from "./src/components/ProfileScreen";
+import MyGames from "./src/components/MyGames"
+import SearchGames from "./src/components/SearchGames"
 
 const Drawer = createDrawerNavigator();
 
@@ -15,6 +17,8 @@ const App = () => {
       <Drawer.Navigator>
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Profile" component={ProfileScreen} />
+        <Drawer.Screen name="My Games" component={MyGames} />
+        <Drawer.Screen name="Search for Games" component={SearchGames} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
