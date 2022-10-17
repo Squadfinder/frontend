157ba469dc07e59fb55ff990cadb9ca1337d41cd
Anna Spitz <aspitzemail@gmail.com>
2022-10-17 11:20:24 -0500

    Add base GameDetailsScreen layout

--- src/components/GameDetailsScreen.js
+++ src/components/GameDetailsScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, ScrollView, Image, StyleSheet, Text } from "react-native";
+import { View, ScrollView, Image, StyleSheet, Text, Pressable } from "react-native";
 
 import gameDetails from "../../mock-data/gameDetails";
-import formatDescription from "../utilities/formatDescription";
 
 const GameDetailsScreen = () => {
-  const { title, image, genres, consoles, description } = gameDetails;
+  const { title, image, genres, consoles } = gameDetails;
+  console.log(consoles)
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.imageContainer}>
@@ -14,15 +14,24 @@ const GameDetailsScreen = () => {
       <View style={styles.titleContainer}>
         <Text style={styles.title}>{title}</Text>
       </View>
-      <View style={styles.descriptionContainer}>
-        <Text style={styles.description}>{description}</Text>
-      </View>
       <View style={styles.genreContainer}>
         {genres.map((genre) => (
           <View style={styles.genreView} key={genre}>
             <Text style={styles.genre}>{genre}</Text>
           </View>
         ))}
+      </View>
+      <View style={styles.consoleContainer}>
+        {consoles.map((console) => (
+          <View style={styles.consoleView} key={console}>
+            <Text style={styles.console}>{console}</Text>
+          </View>
+        ))}
+      </View>
+      <View style={styles.favoriteBtnContainer}>
+        <Pressable style={styles.favoriteBtn}>
+          <Text style={styles.favoriteBtnText}>Favorite Game</Text>
+        </Pressable>
       </View>
     </ScrollView>
   );
@@ -31,8 +40,8 @@ const GameDetailsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#201626",
-    height: "100%",
     alignItems: "center",
+    minHeight: "100%",
   },
   imageContainer: {
     backgroundColor: "#352540",
@@ -63,32 +72,19 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
   },
-  descriptionContainer: {
-    backgroundColor: "#393051",
-    width: "90%",
-    marginTop: 10,
-    borderRadius: 20,
-    shadowColor: "black",
-    shadowRadius: 5,
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 60,
-  },
-  description: {
-    color: "#fff",
-    padding: 20,
-    fontSize: 15,
-    textAlign: "center",
-  },
   genreContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
     width: "90%",
-    marginTop: 5,
+    marginTop: 15,
   },
   genreView: {
     backgroundColor: "#5462A4",
     margin: 5,
-    padding: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingRight: 8,
+    paddingLeft: 8,
     borderRadius: 20,
     shadowColor: "black",
     shadowRadius: 5,
@@ -97,8 +93,62 @@ const styles = StyleSheet.create({
   },
   genre: {
     color: "#fff",
-    fontSize: 15
-  }
+    fontSize: 15,
+  },
+  consoleContainer: {
+    backgroundColor: "#393051",
+    width: "90%",
+    marginTop: 15,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 15,
+    paddingLeft: 15,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    borderRadius: 30,
+    shadowRadius: 2,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 40,
+  },
+  consoleView: {
+    backgroundColor: "#000",
+    margin: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#3AE456",
+  },
+  console: {
+    color: "#fff",
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingRight: 8,
+    paddingLeft: 8,
+  },
+  favoriteBtnContainer: {
+    marginTop: "auto",
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "#000",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  favoriteBtn: {
+    backgroundColor: "#393051",
+    alignSelf: "flex-end",
+    marginTop: "auto",
+    marginBottom: "auto",
+    marginRight: "5%",
+    borderRadius: 30,
+  },
+  favoriteBtnText: {
+    color: "#3AE456",
+    fontSize: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    paddingRight:20,
+  },
 });
 
 export default GameDetailsScreen;
--- src/utilities/formatDescription.js
+++
@@ -1,5 +0,0 @@
-const formatDescription = (description) => {
-    return description.split("<p>").join("<Text>").split("</p>").join("</Text>").split("/n").join("");
-};
-
-export default formatDescription;
